648c69670cd9f6a5fa2ecacfad7e24a1bbccd967
kinogam <kinogam@gmail.com>
2011-09-20 20:04:01 +0800

    add demo

---
+++ Window/demo/window_pkg.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../../Require/Inner/Require.js" />
+kino.Package("window_pkg");
+kino.Require("../css/kino.Window.css", "kino.window.css");
+kino.Require("../../Script/jquery-1.6.1.min.js", "jquery");
+kino.Require("../jquery.kDrag.js", "jquery.kDrag");
+kino.Require("../Window.js", "kino.window.js");
